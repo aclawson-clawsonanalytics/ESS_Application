18ce1419f05b9015c2e329821f6e6046bd34d44c
Andrew Clawson <andrewjclawson18@gmail.com>
2016-06-01 00:03:31 -0600

    Add Campus class.

---
+++ src/java/com/clawsonanalytics/MAX/App/ModelLayer/Campus.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.clawsonanalytics.MAX.App.ModelLayer;
+
+import com.clawsonanalytics.MAX.App.ModelLayer.Interface.IValidatable;
+import com.clawsonanalytics.MAX.App.DataLayer.MySQL.Interface.SQLModel;
+import com.clawsonanalytics.MAX.App.DataLayer.MySQL.MySQLManager;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.util.Calendar;
+import java.util.List;
+import java.util.ArrayList;
+/**
+ *
+ * @author andrewclawson
+ */
+public class Campus extends SQLModel {
+    
+    // class properties
+    
+    public Campus(){
+        
+    }
+    
+    
+}
